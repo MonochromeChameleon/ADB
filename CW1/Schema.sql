8391f47fd9e36e57cd7f434f9edb46f359f806f7
Hugh Greenish <github@monochromechameleon.com>
2014-10-21 13:52:55 +0100

    More tables and notes

--- CW1/Schema.sql
+++ CW1/Schema.sql
@@ -1,14 +1,24 @@
-CREATE TABLE drivers (
+CREATE TABLE employee (
 	id int auto_increment primary key,
+	name varchar(150) not null,
 	address text, -- Oracle: nclob
 	phone_number varchar(20),
 	start_date date not null,
-	end_date date
+	end_date date,
+	job ENUM('driver', 'operator')
 );
 
 -- Trigger: before insert set start date to Now() if not already defined
+-- Validation constraint: max 8 operators
 
-CREATE TABLE cars (
+CREATE TABLE client (
+	id int auto_increment primary key,
+	name varchar(150) not null,
+	phone_number varchar(20) not null,
+	client_type ENUM('corporate', 'private')
+);
+
+CREATE TABLE car (
 	registration varchar(10) not null primary key,
 	registration_date date not null,
 	last_mot date,
@@ -17,4 +27,28 @@ CREATE TABLE cars (
 );
 
 -- Trigger: before insert set start date to Now() if not already defined
+-- FK owner_id should reference a person.
+
+CREATE TABLE booking (
+	id int auto_increment primary key,
+	driver_id int not null,
+	client_id int null,
+	client_name varchar(150) not null,
+	client_phone_number varchar(20) not null,
+	pickup_location text not null,
+	pickup_time datetime not null default CURRENT_TIMESTAMP
+);
+
+-- FK driver_id references employee
+-- Validation constraing - driver_id must not reference an operator
+-- FK client_id references client
+-- Trigger when client_id is not null, client_name/phone_number populates from client table
+
+CREATE TABLE payment (
+	booking_id int not null,
+	payment_method enum('cash', 'account')
+);
+
+-- Validation: account only permitted when booking has a client_id
+
 
