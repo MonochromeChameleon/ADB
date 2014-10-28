0e1835507a89d8fb9504be31f574ce6132f0b691
Hugh Greenish <github@monochromechameleon.com>
2014-10-28 14:50:22 +0000

    Start on data population

--- CW1/Schema.sql
+++ CW1/Schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE car (
     owner_id int not null
 );
 
--- Trigger: before insert set start date to Now() if not already defined
+-- Trigger: before insert set registration date to Now() if not already defined
 -- FK owner_id should reference a person.
 
 CREATE TABLE booking (
@@ -71,5 +71,28 @@ CREATE TABLE shift (
 -- > Get drivers on shift for given date/time
 -- > Get operators on shift for given date/time
 -- > Take payment
+
+-- Data! --
+
+INSERT INTO employee (name, address, phone_number, job) VALUES
+  ('James Jameson', '123 Road Street, London, N1 1NN', '0207123456', 'operator'),
+  ('Robert Robertson', 'Flat 1, Flat Street Apartments, Flat Street, London, E6 6EE', '07777777777', 'operator'),
+  ('Michael Michaels', 'The bungalow, Lane street, London, SW18 18WS', '+44208999999', 'operator'),
+  ('Andy Andrews', 'Our house, In the middle of our street', '+39728739992', 'operator'),
+  
+  ('Bilbo Baggins', 'Bag End, Hobbiton, Middle Earth', '+897838792874', 'driver'),
+  ('Smaug', 'The lonely mountain', '+666666666', 'driver'),
+  ('Batman', 'Wayne Manor, Near Gotham', 'Red phone', 'driver'),
+  ('Superman', 'Fortress of solitude, The Arctic', '+1-800-SUPERMAN', 'driver'),
+  ('Wolverine', 'The Xavier School for Gifted Children', '01234567890', 'driver'),
+  ('Apocalypse', 'Egypt', '00000000000', 'driver');
+
+INSERT INTO car (registration, registration_date, last_mot, car_status, owner_id) VALUES
+  ('AB14 ABC', TO_DATE('2014-05-01'), null, 'roadworthy', 5),
+  ('RRRRRRRR', TO_DATE('2001-01-01'), TO_DATE('2014-01-01'), 'roadworthy', 6),
+  ('BAT 1', null, null, 'roadworthy', 7),
+  ('SUPES', null, null, 'roadworthy', 8),
+  ('X12 XXX', TO_DATE('2012-04-06'), TO_DATE('2014-03-31'), 'awaiting repair', 9),
+  ('NS 4BANR', TO_DATE('0000-01-01'), TO_DATE('2014-01-01'), 'roadworthy', 10);
 
 
