0315718656d0d5706c8864d5d86420c0dfc0a742
Hugh Greenish <github@monochromechameleon.com>
2014-12-09 13:02:07 +0000

    CW2 beginnings

---
+++ CW2/README.md
@@ -0,0 +1,3 @@
+# CW2
+
+a [Sails](http://sailsjs.org) application
---
+++ CW2/api/controllers/BookingController.js
@@ -0,0 +1,11 @@
+/**
+ * BookingController
+ *
+ * @description :: Server-side logic for managing bookings
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
